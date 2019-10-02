c42f21c65c63b901f7caf79f5da405f062c4d6d7
AbdeenM <abdeen.mohamed@outlook.com>
2019-10-02 10:31:58 +0000

    Fixed config file settings

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,5 @@
 export default {
 	SERVER_URL: 'YOUR_SERVER_URL',
 	DB_URL: 'mongodb://localhost/hobee',
-	JWT_SECRET: 'iuwbgu9bg98whwiv9wg98wghne908hgoin',
-
-	// AWS
-	AWS_ACCESS_KEY: 'YOUR_AWS_ACCESS_KEY',
-	AWS_SECRET_KEY: 'YOUR_AWS_SECRET_KEY'
+	JWT_SECRET: 'iuwbgu9bg98whwiv9wg98wghne908hgoin'
 };
